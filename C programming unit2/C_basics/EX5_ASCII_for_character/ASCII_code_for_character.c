4912fd17fb3918c3e37e3bb49b109d875409a938
unknown <ahmedfarid12377@gmail.com>
2025-10-30 00:52:47 +0300

    resolving conflict of renaming file from windwos

--- C programming unit2/C_basics/EX5_ASCII_for_character/ASCII_code_for_character.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void){
-	char ch;
-	scanf("%c",&ch);
-	printf("ASCII code for the character is %i",ch);
-	return 0;
-
-}
--- Embedded C unit3/lesson 2/app.c
+++
@@ -1,6 +0,0 @@
-#include "uart.h"
-unsigned char string_buffer[100]="learn-in-depth:<Ahmed Farid>";
-int  main(void){
-	uart_send_string(string_buffer);
-	return 0;
-}
